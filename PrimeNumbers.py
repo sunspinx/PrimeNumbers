a8700ca0ecb34fa201cbd29af0afa940b1fb0f1e
Vladislav Domin <sunspinx@gmail.com>
2016-11-27 20:48:32 +0100

    User now chooses the max

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,12 +1,12 @@
 import os
+import sys
+import time
 from sys import stdout
 
 # Default values
-num_max = 25000
 num_min = 2
-first_number_in_line = True
-last_number_in_line = False
 numbers = []
+prime_numbers_found = 0
 i = 0
 
 # if text file already exists
@@ -28,6 +28,25 @@ if os.path.exists('primenumber_list.txt'):
     # we iterate it by 1 so we won't check for the same number again
     num_min = max(numbers)
     num_min += 1
+
+print('We are finding prime number from number %d' % num_min)
+# user is able to choose the maximum number we are looking for
+num_max = int(input('Write in the maximum number we are looking for: '))
+
+# if max is lower than min
+if num_max <= num_min:
+    print('You must enter number higher than the minimum')
+    input("Press enter to terminate this file!")
+    # terminate application
+    sys.exit()
+
+print('Starting the script ...')
+# gives it a better look :D
+time.sleep(2)
+
+# clear the console
+clear = lambda: os.system('cls')
+clear()
 
 # loop starts from the number_minimum which is the biggest one from text file or the default value
 for number in range(num_min, num_max+1):
@@ -51,15 +70,18 @@ for number in range(num_min, num_max+1):
             f.write('%d,' % number)
         # iterate variable
         i += 1
+        prime_numbers_found += 1
         # close the file
         f.close()
 
     # show current number
-    stdout.write("\rChecking number: " + str(number) + " ...")
+    stdout.write("\rChecking number: " + str(number) + " of " +str(num_max))
     stdout.flush()
 
-stdout.write("\rApplication checked " + str(num_max - num_min) + " numbers\n")
-print("Text file with all prime number is being opened")
+stdout.write("\rApplication checked " + str(num_max - num_min) + " numbers and found " + str(prime_numbers_found) + " prime numbers.\n")
+print("Text file with all prime number is being opened ...")
+# gives it a better look :D
+time.sleep(2)
 ## open text file with primenumbers
 os.startfile('primenumber_list.txt')
-input("Press any button to terminate this file!")
+input("Press enter to terminate this file!")
